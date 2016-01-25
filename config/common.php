fdc732b4229315e65269e23afcbb1b05fd3a6799
Mike Smith <mike.smith@camc-ltd.co.uk>
2016-01-25 10:01:44 +0000

    + remove demo dashboard

--- config/common.php
+++ config/common.php
@@ -2,20 +2,14 @@
 return array(
     'params' => array(
         'dashboard_items' => array(
-
             array(
                 'module' => 'OphCoMessaging',
                 // default action is the 'renderDashboard' if 'actions' array is  not set
                 'actions' => array(
                     'getInboxMessages',
-                    'getSentMessages',
+                    'getSentMessages'
                  )
-            ),
-            array(
-                'title' => "Demo Dashboard Widget",
-                'content' => "<i>This is a placeholder widget to allow users to try out the drag and drop functionality</i>"
             )
-
         )
     )
 );
